979c1e8d43c04d9e04a94477ed4abfaca52c0c4f
Wilfredo Alcala <alcalawilfre@gmail.com>
2020-03-12 16:29:31 -0300

    Added BuyCheap strategy, cancel expire function

--- src/@types/types.ts
+++ src/@types/types.ts
@@ -1,3 +1,33 @@
-export interface someType {
+import { OrderPost } from '@bitmexInterfaces';
 
+export interface ICandle {
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+}
+
+// Best market prices
+export interface IBestPrice {
+  ask: number;
+  bid: number;
+}
+export interface IMarketData {
+  candles: ICandle[];
+  bestPrice: IBestPrice;
+}
+
+export const MarketSide = {
+  long: 1,
+  short: -1,
+};
+
+export interface IStrategyOrder {
+  symbol: string;
+  side: number; // 1 BUY -1 SELL
+  price?: number;
+  amount?: number;
+  stop?: number;
+  target?: number;
+  expiration: number;
 }
---
+++ src/Strategy/strategies/BuyCheap.ts
@@ -0,0 +1,26 @@
+import { IStrategyOrder, MarketSide, IMarketData } from '@types';
+import StrategyBase from './StrategyBase';
+/**
+ * Esta estrategia manda una orden de compra lejos del bid y pone un expire de 5 segundos para que
+ * la orden sea cancelada 5 segundos luego de ser posteada
+ */
+
+// TODO: Hacer una clase padre para todas las estrategias
+export class BuyCheap extends StrategyBase {
+  constructor(expiration?: number) {
+    expiration = 5000;
+    super(expiration);
+  }
+
+  generateOrder(symbol: string, { candles, bestPrice }: IMarketData) {
+    const price = bestPrice.bid * 0.9; // 10% below market price
+
+    const order: IStrategyOrder = {
+      price,
+      symbol,
+      side: MarketSide.long,
+      expiration: this.expiration,
+    };
+    return order;
+  }
+}
