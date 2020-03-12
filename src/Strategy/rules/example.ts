79f23888d571d42147b4dc28beacef0224fffe32
Wilfredo Alcala <alcalawilfre@gmail.com>
2020-03-12 16:29:51 -0300

    Add rules example

---
+++ src/Strategy/rules/example.ts
@@ -0,0 +1,3 @@
+export const isMarketInBullishExample = (someVariable: any) => {
+  return true;
+};
---
+++ src/Strategy/rules/index.ts
@@ -0,0 +1,7 @@
+/**
+ * La idea de este módulo 'rules' es tener relgas que reciban parámetros, y retornen true o false
+ * en funcion de los cálculos que haga sobre esos parámetros. Las funciones deben ser funciones puras y 
+ * siempre deben responder con true o false
+ */
+
+export * from './example';
