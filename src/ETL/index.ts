5a142768094c06b20ebd05118aceb4b036c553d6
Wilfredo Alcala <alcalawilfre@gmail.com>
2020-03-15 19:22:22 -0300

    Add ETL module - historical data

---
+++ src/ETL/index.ts
@@ -0,0 +1,6 @@
+/**
+ * Este módulo hace uso tanto de la API del cliente Bitmex como del websocket. Sus funciones son:
+ * Bajar la data de bitmex
+ *
+ */
+export * from './ETL';
--- src/start.ts
+++ src/start.ts
@@ -1,9 +1,10 @@
-import { BitmexAPI, BitmexOptions } from '@bitmex';
+import { BitmexAPI, BitmexOptions, BitmexSocket } from '@bitmex';
 import { config } from '@config';
 import app from './API/server';
 import { logger } from '@shared';
 import { gettersService, operationsService } from '@services';
 import * as Strategy from '@Strategy';
+import { ETL } from '@etl';
 
 const options: BitmexOptions = {
   apiKeyID: config.bitmexKeyId,
@@ -11,19 +12,32 @@ const options: BitmexOptions = {
   testnet: config.useTestnet,
 };
 
+// Init BitMEX Client
 const bitmexClient = new BitmexAPI(options);
-
 gettersService.setDependencies(bitmexClient);
 operationsService.setDependencies(bitmexClient);
 
+// Init BitMEX WebSocket
+const bitmexWS = new BitmexSocket(options);
+
+// Init ETL
+const bitmexETL = new ETL(bitmexWS, bitmexClient);
+
+// ETL Example
 (async () => {
-  console.log('Hello world');
+  const marketData = await bitmexETL.getMarketData('XBTUSD', '1m', 10);
+  logger.debug('MarketData:', marketData);
 })();
 
+// Init API
 app.listen(config.serverPort, () => {
   logger.info('Express server started on port: ' + config.serverPort);
 });
 
+// Init Strategy cycle
+// Strategy.start();
+
+/* ------------------------------------------------------------ */
 process
   .on('unhandledRejection', (reason, p) => {
     console.error(reason, 'Unhandled Rejection at Promise', p);
@@ -32,5 +46,3 @@ process
     console.error(err, 'Uncaught Exception thrown');
     process.exit(1);
   });
-
-Strategy.start();
